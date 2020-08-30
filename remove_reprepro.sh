aae299b21f401095dfe6226d113f16ffdcaad9ba
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-08-29 23:11:56 -0300

    agora apos o final teremos um arquivo index
    Agora teremos um arquivo index.html para não ficar mostrando um error 404, mais algumas coisa será adicionado ...

--- remove_reprepro.sh
+++ remove_reprepro.sh
@@ -2,7 +2,68 @@
 cd /aptly/public/
 rm -r db/
 rm -r conf/
+echo "
+<html>
+    <head>
+        <title>Olâ Visitante - Hello visitor</title>
+        <style>
+            body{
+                margin-top: 8%;
+                margin-left: 5%;
+                margin-right: 10%;
+                background-color: white;
+            }
+            p{
+                color: Black
+            }
+            .info{
+                color: red;
+            }
+            .listen{
+                background-color: grey;
+                color: black;
+                border-radius: 15px;
+                align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
+            <p>
+                Português: <a class="info">Por questão de limitação do Github/Gitlab Pages Não podemos mostrar os diretorios</a>
+            </p>
+            <p>
+                English: <a class="info">Due to limitation of Github/Gitlab Pages We cannot show the directories</a>
+            </p>
+            <center>
+                <div class="listen">
+                    <p>
+                        <a>package list | lista de pacotes</a>
+                    </p>
+                    <p>--------------------------</p>
+" > /aptly/public/
+
+for a in $()
+do 
+echo "
+                    <p>
+                        <a>---------------</a>
+                        <a href=${a}>${a}</a>
+                    </p>
+" >> /aptly/public/
+done
+
+echo "
+
+                </div>
+            </center>
+        </main>
+    </body>
+</html>
+" >> /aptly/public/
+
+# Criando algumas pastas e publicando
 mkdir -p /public
 cp -rf ./ /public
 mkdir -p $WORKDIR_SH23/public
-cp -rf $WORKDIR_SH23/public/
+cp -rf . $WORKDIR_SH23/public
