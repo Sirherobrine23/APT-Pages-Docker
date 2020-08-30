b5f888c157e4e0f598a3b551518ddd1bb2bc9747
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-08-29 22:13:53 -0300

    Update publish_reprepro.sh

--- publish_reprepro.sh
+++ publish_reprepro.sh
@@ -4,6 +4,6 @@ cd /aptly/
 echo "Criando um repositorio"
 aptly repo create -config="/aptly/aptly.conf" $DIST 
 echo "Adicionando os arquivos"
-aptly repo add -config="/aptly/aptly.conf" $DIST /aptly/package/main/*.deb
+aptly repo add -config="/aptly/aptly.conf" $DIST /aptly/package/*/*.deb
 echo "publicando"
 aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -config="/aptly/aptly.conf" -distribution=$DIST $DIST
