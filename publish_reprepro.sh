18b9c0f8aaae5238facc3c694dcfa52810bd7ba8
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-08-29 22:13:47 -0300

    mantendo tudo em unico repositorio

--- publish_reprepro.sh
+++ publish_reprepro.sh
@@ -1,14 +1,9 @@
 #/bin/bash
 cd /aptly/
-
-aptly repo create -component=main -config="/aptly/aptly.conf" $DIST 
-aptly repo create -component=contrib -config="/aptly/aptly.conf" $DIST 
-aptly repo create -component=non-free -config="/aptly/aptly.conf" $DIST 
-
-aptly repo add $DIST /aptly/package/main/* -component=main -config="/aptly/aptly.conf" $DIST
-aptly repo add $DIST /aptly/package/contrib/* -component=contrib -config="/aptly/aptly.conf" $DIST
-aptly repo add $DIST /aptly/package/non-free/* -component=non-free -config="/aptly/aptly.conf" $DIST
-
-aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -component=main -config="/aptly/aptly.conf" -distribution=$DIST $DIST
-aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -component=contrib -config="/aptly/aptly.conf" -distribution=$DIST $DIST
-aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -component=non-free -config="/aptly/aptly.conf" -distribution=$DIST $DIST
+#
+echo "Criando um repositorio"
+aptly repo create -config="/aptly/aptly.conf" $DIST 
+echo "Adicionando os arquivos"
+aptly repo add -config="/aptly/aptly.conf" $DIST /aptly/package/main/*.deb
+echo "publicando"
+aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -config="/aptly/aptly.conf" -distribution=$DIST $DIST
--- remove_reprepro.sh
+++ remove_reprepro.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd public/
+cd /aptly/public/
 rm -r db/
 rm -r conf/
 mkdir -p /public
 cp -rf ./ /public
+mkdir -p $WORKDIR_SH23/public
+cp -rf $WORKDIR_SH23/public/
