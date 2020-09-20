6239ce3c616efbb178644782efb486029469d2db
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-09-20 11:08:23 -0300

    Update publish_reprepro.sh

--- publish_reprepro.sh
+++ publish_reprepro.sh
@@ -2,23 +2,18 @@
 cd /aptly/
 for as in $(ls /aptly/package/)
 do
-aptly repo create -distribution=$DIST -component=$as soft-$as
+aptly repo create -distribution=$DIST -component=soft-$as $as
 aptly repo add  soft-$as /aptly/package/$as/*.deb
 
 if [ -z $cop ] ;then
-  cop="soft-$as"
+  cop="$as"
 else
-  cop="$cop soft-$as"
+  cop="$cop $as"
 fi
 if [ -z $cop2 ] ;then
-  cop2="soft-$as"
+  cop2="$as"
 else
-  cop2="$cop2,soft-$as"
-fi
-if [ -z $virg ] ;then
-  echo
-else
-  virg="$virg,"
+  cop2="$cop2,$as"
 fi
 done
 
