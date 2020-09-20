d81b720285e3af498ea098888a8a4a11b5a35121
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2020-09-20 12:29:25 -0300

    Teste haaaaaaaaa

--- remove_reprepro.sh
+++ remove_reprepro.sh
@@ -7,5 +7,5 @@ gpg --armor --output /aptly/public/Release.gpg --export $KEY_ID
 # Criando algumas pastas e publicando
 mkdir -p /public
 mkdir -p $WORKDIR_SH23/public
-cp -rf /aptly/public/ /public
-cp -rf /aptly/public/ $WORKDIR_SH23/public
+cp -rf /aptly/public/* /public
+cp -rf /aptly/public/* $WORKDIR_SH23/public
