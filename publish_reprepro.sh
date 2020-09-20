07ade7c8f4eacc3db275f1e691bca7e8c0584bf7
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2020-09-19 21:44:30 -0300

    test

--- publish_reprepro.sh
+++ publish_reprepro.sh
@@ -1,9 +1,22 @@
 #/bin/bash
 cd /aptly/
-#
-echo "Criando um repositorio"
-aptly repo create -config="/aptly/aptly.conf" $DIST 
-echo "Adicionando os arquivos"
-aptly repo add -config="/aptly/aptly.conf" $DIST /aptly/package/*/*.deb
-echo "publicando"
-aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -config="/aptly/aptly.conf" -distribution=$DIST $DIST
+for as in $(ls /aptly/package/)
+do
+aptly repo create -config="/aptly/aptly.conf" -component="$as" -distribution="$DIST" $as
+
+aptly repo add -config="/aptly/aptly.conf" $as /aptly/package/$as/*.deb
+
+cop="$cop $as"
+done
+
+aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -config="/aptly/aptly.conf" -components=, $cop
+
+
+
+
+
+#echo "Criando um repositorio"
+#aptly repo create -config="/aptly/aptly.conf" $DIST 
+#echo "Adicionando os arquivos"
+#echo "publicando"
+#aptly publish repo -passphrase="$pass" -batch -force-overwrite=true -config="/aptly/aptly.conf" -distribution=$DIST $DIST
