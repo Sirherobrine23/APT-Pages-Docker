8c72ff1b1d933085ada522518b57beacfe974d59
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2020-09-23 20:56:52 -0300

    Update remove_reprepro.sh

--- remove_reprepro.sh
+++ remove_reprepro.sh
@@ -4,6 +4,12 @@ cd /aptly/public/
 # Key
 gpg --armor --output /aptly/public/Release.gpg --export $KEY_ID
 
+echo "
+    echo 'GPGKEY' | apt-key add -
+    echo deb URLADD URLMAIN
+    apt update
+" | sed "s|GPGKEY|$(cat Release.gpg)|g" | sed "s|URLADD|$URL_REPO|g" | sed "s|URLMAIN|$(ls pool/)|g" > add-repo.sh
+
 # Criando algumas pastas e publicando
 mkdir -p /public
 mkdir -p $WORKDIR_SH23/public
